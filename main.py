59515d84f2c50ffda9cb1a0f3af03eab8d023ae3
o46 <bIXV904O&V6z67VRm%cQf78fTe@t*gzw>
2021-09-16 11:47:55 -0600

    modified     ShowPicker         added menu definition, laying groundwork for adding / removing shows

--- main.py
+++ main.py
@@ -25,17 +25,38 @@ class ShowPicker:
         self.shows = load_file("resources", "shows.txt")
         self.colors = load_file("resources", "colors.txt")
 
-
     def runner(self):
         while True:
             random.shuffle(self.shows)
             random.shuffle(self.colors)
             picked = input("Pick a color: \n\t" + "\n\t".join(self.colors[:len(self.shows)]) + "\n").strip().capitalize()
+            print(picked)
             if picked in self.colors[:len(self.shows)]:
                 print("Your show is: " + self.shows[self.colors.index(picked)] + "\n" * 5)
+            elif picked == "Exit":
+                break
             else:
                 input("Your selection, " + picked + ", was not found in the color list. Press enter to continue." + "\n" * 5)
 
+    def menu(self):
+        while True:
+            menu_choice = input("[1]\tPick a show\n[2]\tAdd a show\n[3]\tDelete a show\n[4]\tExit\n").strip()
+            try:
+                menu_choice = int(menu_choice)
+                print(menu_choice)
+                if menu_choice == 1:
+                    self.runner()
+                elif menu_choice == 2:
+                    print("Not implemented yet.")
+                elif menu_choice == 3:
+                    print("Not implemented yet.")
+                elif menu_choice == 4:
+                    sys.exit(0)
+                else:
+                    print("Please choose one of the listed options.")
+            except SystemExit:
+                sys.exit(0)
+
 
 if __name__ == "__main__":
-    ShowPicker().runner()
+    ShowPicker().menu()
